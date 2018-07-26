a7cec8b676a9dfbe86f366f88795ae5eaf775afd
zardyang <2634270637@qq.com>
2018-07-26 12:41:19 +0800

    merge dev

---
+++ ssmDemo/src/main/java/com/just/demo/exception/NoNumberException.java
@@ -0,0 +1,15 @@
+package com.just.demo.exception;
+/**
+ * ¿â´æ²»×ãÒì³£
+ */
+public class NoNumberException extends RuntimeException {
+
+    public NoNumberException(String message) {
+        super(message);
+    }
+
+    public NoNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
